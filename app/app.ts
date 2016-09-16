0d65629acd2228c054bd44f1ce44c9c609c21aa5
EliasSimoes <eliasunipdesign@gmail.com>
2016-09-16 18:00:58 -0300

    Alteração do menu, criando uma collection das pages e fazendo um binding com *ngFor

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Platform, ionicBootstrap, MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 import {IngredientesPage} from './pages/ingredientes/ingredientes';
@@ -18,30 +18,47 @@ import {LoginPage} from './pages/login/login';
 @Component({
   templateUrl: 'build/app.html'
 })
+
 export class MyApp {
   
-  login: any = LoginPage;
-  testes: any = TestesPage;
-  indique: any = IndiquePage;
-  conquistas: any = ConquistasPage;
-  buscar: any = BuscarPage;
-  historico: any = HistoricoPage;
+  // login: any = LoginPage;
+  // testes: any = TestesPage;
+  // indique: any = IndiquePage;
+  // conquistas: any = ConquistasPage;
+  // buscar: any = BuscarPage;
+  // historico: any = HistoricoPage;
   ingredientes: any = IngredientesPage;
   home: any = HomePage;
   
+  pages: Array<any>;
+  
   rootPage: any = this.home;
 
-  constructor(platform: Platform) {
+  constructor(public platform: Platform
+              , public menuCtrl: MenuController) {
+
+    this.pages = [
+      { title:'Testes', component: TestesPage },
+      { title:'Ingredientes', component: IngredientesPage },
+      { title:'Buscar receitas', component: BuscarPage },
+      { title:'Histórico de receitas', component: HistoricoPage },
+      { title:'Conquistas', component: ConquistasPage },
+      { title:'Indique à um amigo', component: IndiquePage },                  
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       console.log('App rodando ok!!!');
       StatusBar.styleDefault();
     });
+
+
   }
 
   openPage(page){
-    this.rootPage = page;
+    this.menuCtrl.close();
+    this.rootPage = page.component;
   }
 }
 
